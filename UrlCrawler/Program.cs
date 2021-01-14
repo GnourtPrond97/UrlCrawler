ee931e260d2dd09c1ebe2cec5be76673f6a673dd
GnourtPrond97 <truonghhth1905001@gmail.com>
2021-01-15 03:22:07 +0700

    first

---
+++ UrlCrawler/Program.cs
@@ -0,0 +1,70 @@
+﻿using HtmlAgilityPack;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var doc = new HtmlWeb().Load("https://vnexpress.net/suc-khoe/dinh-duong");
+            var doc = new HtmlWeb().Load("https://vnexpress.net/suc-khoe/dinh-duong");
+            //var linkTags = doc.DocumentNode.Descendants("link");
+            var linkedPages = doc.DocumentNode.Descendants("a")
+                                              .Select(a => a.GetAttributeValue("href", null))
+                                              .Where(u => !String.IsNullOrEmpty(u));
+            HashSet<string> result = new HashSet<string>();
+            foreach (string item in linkedPages)
+            {   
+                if (item.Contains(".html") && !item.Contains("#box_comment_vne"))
+                {
+                    result.Add(item);
+
+                }
+            }
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+
+                channel.QueueDeclare(queue: "hello",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+                string message = "Chui thecc";
+
+                int count = 0;
+                foreach (string item in result)
+                {
+                    
+
+                    var body = Encoding.UTF8.GetBytes(item);
+                    channel.BasicPublish(exchange: "",
+                                    routingKey: "hello",
+                                    basicProperties: null,
+                                    body: body);
+                    count++;
+                }
+
+
+
+                Console.WriteLine(" [x] Sent " + count);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    
+    
+    }
+}
